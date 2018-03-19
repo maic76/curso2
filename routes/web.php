d0aa36a2a152da98f0e7cbae9a1368a14b116c71
maic76 <maic76@gmail.com>
2018-03-19 17:02:07 -0600

    Cree los Controllers Departamentos, Empleados, Turnos y un grupo de rutas (admin) con un resource de Empleado.

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('empleado/empleados');
+});
+
+
+Route::group(['prefix' => 'admin'],function(){
+
+	Route::resource('empleados','EmpleadosController');
 });
